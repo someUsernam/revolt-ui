f43b9aae5444fdf7e4d319e51ce1533134e0ffa8
Artur Wojtowicz <arturwojtowik@gmail.com>
2024-04-07 11:58:12 +0200

    Refactor: Refactor Button component and vt utility function

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -1,34 +1,41 @@
 import React, { ComponentProps, ElementRef } from "react";
 import { cx } from "../src/utils/cx";
+import { vt } from "../src/utils/vt";
 
 interface ButtonProps extends ComponentProps<"button"> {
 	type?: "button" | "submit" | "reset";
 	variant?: "primary" | "secondary";
 	className?: string;
 	disabled?: boolean;
-	size?: "base" | "sm" | "lg";
+	size?: "md" | "sm" | "lg";
 	buttonRef?: React.RefObject<ElementRef<"button">>;
 	children: React.ReactNode;
 }
 
-const dataHover = "";
-
-const dataActive = "";
-
-const dataFocus = "";
-
-const dataDisabled =
-	"data-[disabled]:opacity-50 after:data-[disabled]:shadow-none before:data-[disabled]:shadow-none";
-
 const after =
 	"after:-z-10 after:absolute after:inset-0 after:rounded-[calc(theme(borderRadius.lg)-1px)]";
 
-const base = `relative rounded-lg inline-flex items-center justify-center gap-x-2 text-base/6 font-semibold isolate px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] sm:px-[calc(theme(spacing.3)-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] sm:text-sm/6 ${after}`;
+const base =
+	"relative rounded-lg inline-flex items-center justify-center gap-x-2 text-base/6 font-semibold isolate px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] sm:px-[calc(theme(spacing.3)-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] sm:text-sm/6";
 
-const variants = {
-	primary: `${base} bg-blue-500 text-white `,
-	secondary: `${base} bg-red-500 text-white`,
-};
+const button = vt([base, after], {
+	variants: {
+		primary: "bg-blue-500 text-white",
+		secondary: "bg-red-500 text-white",
+	},
+	sizes: {
+		sm: "px-[calc(theme(spacing[2])-1px)] py-[calc(theme(spacing[2])-1px)] sm:px-[calc(theme(spacing.2)-1px)] sm:py-[calc(theme(spacing[1])-1px)]",
+		md: "",
+		lg: "text-lg/6 sm:text-lg/6",
+	},
+	states: {
+		hover: "hover:bg-red-600",
+		active: "active:bg-blue-700",
+		focus: "focus:ring-2 focus:ring-blue-500",
+		disabled:
+			"data-[disabled]:opacity-50 after:data-[disabled]:shadow-none before:data-[disabled]:shadow-none",
+	},
+});
 
 function Button({
 	type = "button",
@@ -37,7 +44,7 @@ function Button({
 	className = "",
 	disabled = false,
 	buttonRef,
-	size = "base",
+	size = "md",
 	...props
 }: ButtonProps) {
 	return (
@@ -45,12 +52,12 @@ function Button({
 			type={type}
 			data-variant={variant}
 			className={cx(
+				button({
+					variant,
+					size,
+					disabled,
+				}),
 				className,
-				variants[variant],
-				disabled && dataDisabled,
-				size === "sm" &&
-					"px-[calc(theme(spacing[2])-1px)] py-[calc(theme(spacing[2])-1px)] sm:px-[calc(theme(spacing.2)-1px)] sm:py-[calc(theme(spacing[1])-1px)]",
-				size === "lg" && "text-lg/6 sm:text-lg/6",
 			)}
 			disabled={disabled}
 			data-disabled={disabled}
